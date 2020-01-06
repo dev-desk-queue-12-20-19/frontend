810a4003fc33dde03f0a7fb06a39cab874798aea
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-06 12:29:02 +0000

    Login and register Started

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+import Login from "./components/Login";
+import Registration from "./components/Registration"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-         
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Login />
+      <Registration />
+
     </div>
+    </Router>
   );
 }
 
--- front-end/src/utils/axiosWithAuth.js
+++ front-end/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
     return axios.create ({
-        baseURL: 'https://devdesk-queue.herokuapp.com/api/',
+        baseURL: 'https://devdesk-queue.herokuapp.com/api',
         headers: {
             Authorization: token
         }
