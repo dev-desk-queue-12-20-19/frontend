d59d4d13a13fa34e69dc6b741d01067da1d0ba5d
oredola <oredolaolamide@gmail.com>
2020-01-06 19:24:53 +0100

    Added dashboard, and ticket card. Basic styling.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
+import "./App.css";
 
 import Login from "./components/Login";
 import Registration from "./components/Registration";
