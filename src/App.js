a6b6c2eb22438e9433302fa39b753f0fdc84017d
oredola <oredolaolamide@gmail.com>
2020-01-08 12:25:11 +0100

    WIP: moved browserRouter to index.js.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 
 import "./App.css";
@@ -7,16 +7,16 @@ import "./App.css";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
 import Dashboard from "./components/Dashboard";
+import NewTicket from "./components/NewTicket";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Login} />
+    <div>
+      {/* <Route exact path="/" component={Login} />
         <Route path="/register" component={Registration} />
-        <Route  path="/dashboard" component={Dashboard}/>
-      </div>
-    </Router>
+        <Route  path="/dashboard" component={Dashboard}/> */}
+      <NewTicket />
+    </div>
   );
 }
 
